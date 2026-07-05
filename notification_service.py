8e86a579498902c17b783212f90ba9d3f85174fc
2024ac05306-droid <2024ac05306@wilp.bits-pilani.ac.in>
2026-07-05 23:39:47 +0530

    Update notification_service.py

--- notification_service.py
+++ notification_service.py
@@ -1,1 +1,14 @@
+# notification_service.py
 
+from fastapi import FastAPI
+
+app = FastAPI(title="Notification Service")
+
+@app.post("/notify")
+def notify(data: dict):
+
+    print("Sending Notification:", data)
+
+    return {
+        "message": "Notification sent"
+    }
