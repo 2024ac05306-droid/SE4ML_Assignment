cb39738899db1b0aa9877f94dd7898a9ad0e8667
2024ac05306-droid <2024ac05306@wilp.bits-pilani.ac.in>
2026-07-05 23:57:09 +0530

    Modify load_data to fetch data from a URL
    Updated load_data function to load data from a URL with specified column names.

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,21 @@
-
 # data_loader.py
 
 import pandas as pd
 
-def load_data(path):
-    return pd.read_csv(path)
+def load_data():
+
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
+
+    columns = [
+        "Pregnancies",
+        "Glucose",
+        "BloodPressure",
+        "SkinThickness",
+        "Insulin",
+        "BMI",
+        "DiabetesPedigreeFunction",
+        "Age",
+        "Outcome"
+    ]
+
+    return pd.read_csv(url, names=columns)
