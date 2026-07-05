ae06732a19a6910854c48d4eb2fc79c1fe99f2e5
2024ac05306-droid <2024ac05306@wilp.bits-pilani.ac.in>
2026-07-06 00:09:37 +0530

    Load dataset from URL and update model training

--- train_model.py
+++ train_model.py
@@ -2,34 +2,38 @@
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from data_loader import load_data
+import pandas as pd
 import joblib
 import os
 
-# Load dataset 
-df = load_data()
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
 
-# Features and target
+columns = [
+    "Pregnancies",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPedigreeFunction",
+    "Age",
+    "Outcome"
+]
+
+df = pd.read_csv(url, names=columns)
+
 X = df.drop("Outcome", axis=1)
 y = df["Outcome"]
 
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
-# Train model
 model = RandomForestClassifier(random_state=42)
-
 model.fit(X_train, y_train)
 
-# Create models directory if it doesn't exist
 os.makedirs("models", exist_ok=True)
 
-# Save model
 joblib.dump(model, "models/diabetes_model.pkl")
 
-print("Model trained and saved successfully.")
+print("Model trained successfully")
